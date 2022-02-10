2b0b3eb7640ce7d6a9916c167498fccc52126a50
achieuw <achieuw@hotmail.com>
2022-02-10 17:54:00 +0100

    FEAT: Add hero class

---
+++ Hero/Hero.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_RPG_Characters
+{
+    class Hero : BaseHero
+    {
+        public override string Name { get; set; }
+        public override int Level { get; set ; }
+        public struct PrimaryStats
+        {
+            public static int strength = 1;
+            public static int intelligence = 1;
+            public static int dexterity = 1;
+        }
+        public Hero()
+        {
+            Level = 1;
+        }
+        public override void DealDamage()
+        {
+
+        }
+        public override void LevelUp()
+        {
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 ﻿using System;
+/// <summary>
+/// a) Various character classes having attributes which increase at different rates as the character gains levels.
+/// b) Equipment, such as armor and weapons, that characters can equip. The equipped items will alter the power of
+/// the character, causing it to deal more damage and be able to survive longer. Certain characters can equip
+/// certain item types.
+/// c) Summary(///) tags for each method you write, explaining what the method does, any exceptions it can throw,
+/// and what data it returns(if applicable).You do not need to write summary tags for overloaded methods.
+/// d) Custom exceptions.There are two custom exceptions you are required to write, as detailed in Appendix B.
+/// e) Full test coverage of the functionality. Some testing data is provided, it can be used to complete the assignment
+/// in a test-driven development manner.
+/// </summary>
 
 namespace Assignment_1_RPG_Characters
 {
@@ -6,7 +17,8 @@ namespace Assignment_1_RPG_Characters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Mage mage = new Mage("Achieuw");
+            Console.WriteLine($"Name: {mage.Name} Level: {mage.Level}");
         }
     }
 }
