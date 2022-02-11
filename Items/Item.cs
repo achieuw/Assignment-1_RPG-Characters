e042dfbfd2b11fdcc06ccb17715371aafe93062f
achieuw <achieuw@hotmail.com>
2022-02-11 15:52:33 +0100

    CHORE: Add item class

---
+++ Items/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_RPG_Characters.Items
+{
+    abstract class Item
+    {
+        public string Name { get; set; }
+        public abstract int RequiredLevel { get; set; }
+        public abstract int Slot { get; set; }
+    }
+}
