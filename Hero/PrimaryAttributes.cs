77f73e3e79d5fa19479be30f65c4c238b0c8248d
achieuw <achieuw@hotmail.com>
2022-02-11 15:51:57 +0100

    CHORE: Create class for primary attributes

---
+++ Hero/PrimaryAttributes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_RPG_Characters
+{
+    /// <summary>
+    /// Primary base attributes with properties: Strength, Dexterity, Intelligence. Defaults value to 1.
+    /// </summary>
+    class PrimaryAttributes
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+
+        public PrimaryAttributes(int strength, int dexterity, int intelligence)
+        {
+            Strength = strength;
+            Dexterity = dexterity;
+            Intelligence = intelligence;
+        }
+
+        /// <summary>
+        /// Operator overloading for adding primary attributes
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <returns>Sum of primary attributes</returns>
+        public static PrimaryAttributes operator +(PrimaryAttributes p1, PrimaryAttributes p2)
+        {
+            return new PrimaryAttributes(p1.Strength + p2.Strength, p1.Dexterity + p2.Dexterity, p1.Intelligence + p2.Intelligence);
+        }
+    }
+}
