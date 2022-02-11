ed6c92cb2aaaca1150522a33e6a5e7290eb588cc
achieuw <achieuw@hotmail.com>
2022-02-11 15:54:21 +0100

    FEAT: Add mage class

--- Hero/Mage.cs
+++ Hero/Mage.cs
@@ -1,16 +1,20 @@
-﻿using System;
+﻿using Assignment_1_RPG_Characters.Items;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assignment_1_RPG_Characters
 {
     class Mage : Hero
     {
-        public Mage(string name)
+        public override List<Weapon.Types> WeaponProficiencies { get; set; }
+        // Armor proficiencies
+        public Mage(string name) : base(name)
         {
-            Name = name;
+            BaseStats.Intelligence = 8;
+            StatIncreaseOnLevelUp.Intelligence = 5;
+            WeaponProficiencies.Add(Weapon.Types.Wand);
+            WeaponProficiencies.Add(Weapon.Types.Staff);
         }
+        public override int DealDamage() => (int)(Weapon.DPS * (1 + (BaseStats.Intelligence / 100.0)));
     }
 }
