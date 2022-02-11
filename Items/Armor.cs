0628f4067d393643e158391cdc27f9c954079ced
achieuw <achieuw@hotmail.com>
2022-02-11 15:53:32 +0100

    CHORE: Add classes for items

---
+++ Items/Armor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_RPG_Characters.Items
+{
+    class Armor
+    {
+        public PrimaryAttributes Stats { get; set; }
+
+        public Armor()
+        {
+            Stats = new PrimaryAttributes(0, 0, 0);
+        }
+    }
+}
---
+++ Items/Equipment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Assignment_1_RPG_Characters.Items
+{
+    class Equipment
+    {
+        public Dictionary<int, Item> GearSlots;
+
+        public readonly int MaxSlots = 4;
+
+        public Equipment()
+        {
+            GearSlots = new();
+
+            // Add slots for the gear and default items to null
+            for (int i = 1; i <= MaxSlots; i++)
+            {
+                GearSlots.Add(i, null);
+            }
+        }
+
+        public void AddToGearSlot(int slot, Item item)
+        {
+            GearSlots.Add(slot, item);
+        }
+    }
+}
