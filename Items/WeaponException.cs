56c1996f2da20ebded17c6a30311a0f246ff5f13
achieuw <achieuw@hotmail.com>
2022-02-12 10:00:36 +0100

    TEST: add weapon exception

---
+++ Items/WeaponException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCharacters.Items
+{
+    class WeaponException : Exception
+    {
+        public WeaponException(string message) : base(message)
+        {
+        }
+
+        public override string Message => "Weapon Exception";
+    }
+}
