d9109b4a4a2ee29a571333aa9aa59336a0b15070
achieuw <achieuw@hotmail.com>
2022-02-16 15:42:17 +0100

    FEAT: Rogue class

---
+++ Hero/Rogue.cs
@@ -0,0 +1,28 @@
+﻿using RPGCharacters.Items;
+using System.Collections.Generic;
+
+namespace RPGCharacters
+{
+    public class Rogue : Hero
+    {
+        public override List<Weapon.Types> WeaponProficiencies { get; set; }
+        public override List<Armor.Types> ArmorProficiencies { get; set; }
+
+        public Rogue(string name) : base(name)
+        {
+            BaseStats.Strength = 2;
+            BaseStats.Dexterity = 6;
+            StatIncreaseOnLevelUp.Dexterity = 4;
+            WeaponProficiencies.Add(Weapon.Types.Dagger);
+            WeaponProficiencies.Add(Weapon.Types.Sword);
+            ArmorProficiencies.Add(Armor.Types.Leather);
+            ArmorProficiencies.Add(Armor.Types.Mail);
+
+        }
+        public override double DealDamage()
+        {
+            double damage = (Weapon.DPS * (1 + ((BaseStats + GetStats()).Dexterity / 100.0)));
+            return damage;
+        }
+    }
+}
